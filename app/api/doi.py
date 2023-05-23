cfca6e23d348988d8f2d14e61316baa7824a3ada
sam <sam.woodcock@protonmail.com>
2023-05-23 16:17:09 +0100

    refactor: working doi endpoints, withoit datacite handler

--- app/api/doi.py
+++ app/api/doi.py
@@ -67,10 +67,10 @@ async def create_doi_db_only(doi: DoiRealisationInPydantic):
     """Create new doi."""
     log.debug(f"Creating new DOI with params: {doi}")
 
-    doi_exists = await DoiRealisation.filter(
+    doi_exists = await DoiRealisation.get_or_none(
         prefix_id=settings.DOI_PREFIX,
         suffix_id=doi.suffix_id,
-    ).exists()
+    )
 
     if doi_exists:
         HTTPException(status_code=409, detail=f"DOI already exists: {doi}")
@@ -99,12 +99,6 @@ async def create_doi_draft(package_id: str):
     POST is required to prevent conflict with GET /doi/{id}.
     """
     log.debug(f"Creating new draft doi for package id: {package_id}")
-    # Call Datacite handler datacite.py
-    # if errors in datacite return
-    # return JSONResponse(status_code=409)
-
-    # Call DOI minting handler minter.py
-    # Return the __str__ from model for full prefix/suffix
 
     next_id = await get_next_doi_suffix_id()
 
@@ -121,11 +115,25 @@ async def create_doi_draft(package_id: str):
         "ckan_entity": "package",
     }
 
-    log.debug(f"Creating new doi with params: {new_doi}")
-    # dois_obj = await DoiRealisation.create(**new_doi)
-    # return await DoiRealisationInPydantic.from_tortoise_orm(dois_obj)
+    doi_exists = await DoiRealisation.get_or_none(
+        prefix_id=new_doi.prefix_id,
+        suffix_id=new_doi.suffix_id,
+    )
 
-    return await DoiRealisationPydantic.from_queryset_single(DoiRealisation.first())
+    if doi_exists:
+        log.debug("DOI database already exists, continuing to datacite logic")
+    else:
+        log.debug(f"Creating new DOI with params: {new_doi}")
+        await DoiRealisation.create(**new_doi)
+
+    # # Call Datacite draft handler datacite.py
+    # if datacite_error:
+    #     HTTPException(
+    #         status_code=409,
+    #         detail=f"Error with datacite draft creation: {datacite_error}"
+    #     )
+
+    # return await DoiRealisationInPydantic.from_tortoise_orm(dois_obj)
 
 
 @router.put(
@@ -137,6 +145,14 @@ async def update_doi(package_id: str, doi: DoiRealisationEditPydantic):
     """Update specific DOI in DB and Datacite for CKAN Package ID."""
     log.debug(f"Attempting to update doi ID {id} with params: {doi}")
     await DoiRealisation.filter(doi_pk=id).update(**doi.dict(exclude_unset=True))
+
+    # # Call Datacite update handler datacite.py
+    # if datacite_error:
+    #     HTTPException(
+    #         status_code=409,
+    #         detail=f"Error with datacite update: {datacite_error}"
+    #     )
+
     return await DoiRealisationEditPydantic.from_queryset_single(
         DoiRealisation.get(doi_pk=id)
     )
