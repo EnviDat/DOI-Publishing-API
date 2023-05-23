767d42250cc22259901cdd93cc5a1af15f48919b
sam <sam.woodcock@protonmail.com>
2023-05-23 12:45:05 +0100

    refactor: comment out non-functional code, enable piece by piece

--- app/api/approval.py
+++ app/api/approval.py
@@ -1,24 +1,24 @@
-"""Endpoints to trigger emails."""
+# """Endpoints to trigger emails."""
 
-import logging
+# import logging
 
-from fastapi import APIRouter
+# from fastapi import APIRouter
 
-log = logging.getLogger(__name__)
+# log = logging.getLogger(__name__)
 
-router = APIRouter(
-    prefix="/mail",
-    tags=["mail"],
-)
-
-
-@router.get("/request-approval")
-async def get_all_dois():
-    """Request approval to publish."""
-    return
+# router = APIRouter(
+#     prefix="/mail",
+#     tags=["mail"],
+# )
 
 
-@router.get("/approve")
-async def get_all_dois():
-    """Request approval to publish."""
-    return
+# @router.get("/request-approval")
+# async def get_all_dois():
+#     """Request approval to publish."""
+#     return
+
+
+# @router.get("/approve")
+# async def get_all_dois():
+#     """Request approval to publish."""
+#     return
---
+++ app/logic/minter.py
@@ -0,0 +1,1 @@
+"""Mints a new DOI."""
