b392f8dc068797ecc020f3267f03d2959195f123
sam <sam.woodcock@protonmail.com>
2023-05-30 16:34:38 +0100

    fix: remove /approve endpoint, pub status update_requested=updated

--- app/api/approval.py
+++ app/api/approval.py
@@ -7,7 +7,7 @@ from fastapi import APIRouter, Header
 from fastapi.responses import JSONResponse
 
 from app.config import settings
-from app.logic.mail import approval_granted_email, request_approval_email
+from app.logic.mail import request_approval_email
 
 log = logging.getLogger(__name__)
 
@@ -23,7 +23,7 @@ async def request_publish_approval(
 ):
     """Request approval to publish."""
     # Send email to admin
-    request_approval_email()
+    request_approval_email(package_id="", user_email="")
 
     # Requires testing
     log.debug("Initialising CKAN connection")
@@ -31,36 +31,11 @@ async def request_publish_approval(
 
     log.debug(
         f"Updating package {id} to publication_status="
-        f"{'update_pending' if update else 'pub_pending'}"
+        f"{'updated' if update else 'pub_pending'}"
     )
     ckan.call_action(
         "package_update",
-        {"publication_status": "update_pending" if update else "pub_pending"},
+        {"publication_status": "updated" if update else "pub_pending"},
         api_key=authorization,
     )
-    return JSONResponse(status_code=200, content={"success": True})
-
-
-@router.get("/approve")
-async def approve_publishing(package_id: str, authorization: str = Header(None)):
-    """Request approval to publish."""
-    # Requires testing
-    log.debug("Initialising CKAN connection")
-    ckan = ckanapi.RemoteCKAN(settings.API_URL)
-    log.debug(f"Updating package {id} to publication_status=approved")
-    ckan.call_action(
-        "package_update", {"publication_status": "approved"}, api_key=authorization
-    )
-
-    log.debug("Attempting update via Datacite API")
-    # # Call Datacite update handler datacite.py
-    # if datacite_error:
-    #     HTTPException(
-    #         status_code=409,
-    #         detail=f"Error with datacite update: {datacite_error}"
-    #     )
-
-    # Send email to user
-    approval_granted_email()
-
     return JSONResponse(status_code=200, content={"success": True})
