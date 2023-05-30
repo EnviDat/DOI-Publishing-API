866d2941c6d848e6b6c61ed82fa88396918177ff
sam <sam.woodcock@protonmail.com>
2023-05-30 18:30:48 +0100

    fix: working auth functions extracting authorization header

--- app/auth.py
+++ app/auth.py
@@ -1,38 +1,44 @@
 """Validate authorization header and get user details."""
 
 import logging
+from typing import Annotated
 
 import ckanapi
-from fastapi import Header, HTTPException
+from fastapi import Depends, Header, HTTPException
 
 from app.config import settings
 
 log = logging.getLogger(__name__)
 
 
-def get_user(authorization: str = Header(None)):
+def get_user(authorization: Annotated[str | None, Header()] = None):
     """Standard CKAN user."""
-    if not authorization:
+    if not settings.DEBUG and not authorization:
         log.error("No Authorization header present")
         raise HTTPException(status_code=401, detail="No Authorization header present")
+
+    log.warning(settings.DEBUG)
+    log.warning(type(settings.DEBUG))
+
+    log.debug("Authorization header extracted from request headers")
 
     if settings.DEBUG:
         user_info = {
             "id": "334cee1e-6afa-4639-88a2-f980e6ff42c3",
             "name": "admin",
+            "display_name": "EnviDat Admin",
+            "email": "envidat@wsl.ch",
             "sysadmin": True,
         }
     else:
-        ckan = ckanapi.RemoteCKAN(settings.API_URL)
-        user_info = ckan.call_action("user_show", api_key=authorization)
+        ckan = ckanapi.RemoteCKAN(settings.API_URL, apikey=authorization)
+        user_info = ckan.call_action("user_show")
 
     return user_info
 
 
-def get_admin():
+def get_admin(user_info=Depends(get_user)):
     """Admin CKAN user."""
-    user_info = get_user()
-
     # Determine if is an admin
     admin = user_info.get("sysadmin", False)
 
