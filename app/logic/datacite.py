d4153799cb122c2d2dbcc818f9e71d9a939721fc
RebeccaKurupBuchholz <rebecca.kurup@wsl.ch>
2023-06-05 11:58:32 +0200

    feat: add get_package() function

--- app/logic/datacite.py
+++ app/logic/datacite.py
@@ -2,10 +2,39 @@
 
 # TODO review and possibly remove dependencies in lib/envidat
 
-from typing import TypedDict
-from app.config import settings
+from typing import TypedDict, Annotated
 import json
 import requests
+
+from ckanapi import RemoteCKAN, NotFound, NotAuthorized
+from fastapi import HTTPException, Header
+from app.config import settings
+
+import logging
+log = logging.getLogger(__name__)
+
+
+def get_package(package_id: str,
+                authorization: Annotated[str | None, Header()] = None):
+    """Return CKAN package"""
+    if not authorization:
+        log.error("No Authorization header present")
+        raise HTTPException(status_code=401,
+                            detail="No Authorization header present")
+
+    try:
+        ckan = RemoteCKAN(settings.API_URL, apikey=authorization)
+        package = ckan.call_action("package_show", {'id': package_id})
+    except NotFound as e:
+        log.exception(e)
+        raise HTTPException(status_code=404,
+                            detail="Package not found")
+    except NotAuthorized as e:
+        log.exception(e)
+        raise HTTPException(status_code=403,
+                            detail="User not authorized to read package")
+
+    return package
 
 
 class DoiSuccess(TypedDict):
