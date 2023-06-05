89769da79306d20aed78d31f578a749201245fda
RebeccaKurupBuchholz <rebecca.kurup@wsl.ch>
2023-06-05 11:57:07 +0200

    feat: add "/datacite/draft" endpoint and supporting logic

--- app/api/datacite.py
+++ app/api/datacite.py
@@ -6,10 +6,12 @@ from fastapi import APIRouter, Depends, Header, Request
 from typing import Annotated
 
 from app.auth import get_user
-
+from app.logic.datacite import get_package
 
 log = logging.getLogger(__name__)
 
+# TODO improve Swagger documentation of endpoints (title, description,
+#  query parameters etc.)
 
 # TODO implement authorization
 # TODO remove dependencies if unused
@@ -19,8 +21,18 @@ router = APIRouter(prefix="/datacite", tags=["datacite"]
 
 
 # TODO make header auth work in Swagger
-@router.get("")
-async def test(authorization: Annotated[str | None, Header()] = None):
-    """Get all dois."""
-    auth = get_user(authorization)
+# TODO implement error handling if auth not valid
+@router.get("/draft")
+def get_draft_doi(user: str,
+                  package: str,
+                  authorization: Annotated[str | None, Header()] = None):
+    """Reserve draft DOI in DataCite."""
+
+    # Authorize user
+    auth = get_user(user, authorization)
+
+    # TODO test getting package
+    # Get package
+    package = get_package(package, authorization)
+
     return auth
