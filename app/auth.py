cac35b20bdafefe69778bbee1a5b9cf8e1e10f7f
RebeccaKurupBuchholz <rebecca.kurup@wsl.ch>
2023-06-05 11:56:07 +0200

    fix: add id to "user_show" call in auth.get_user

--- app/auth.py
+++ app/auth.py
@@ -1,52 +1,89 @@
 """Validate authorization header and get user details."""
 
-import logging
 from typing import Annotated
 
-import ckanapi
+from ckanapi import RemoteCKAN, NotFound
 from fastapi import Depends, Header, HTTPException
-
 from app.config import settings
+
+import logging
 
 log = logging.getLogger(__name__)
 
 
-def get_user(authorization: Annotated[str | None, Header()] = None):
-    """Standard CKAN user."""
-    if not settings.DEBUG and not authorization:
+# TODO test with admin user
+# TODO review if DEBUG needed
+def get_user(user_id: str,
+             authorization: Annotated[str | None, Header()] = None):
+    """Return CKAN user"""
+    if not authorization:
         log.error("No Authorization header present")
-        raise HTTPException(status_code=401, detail="No Authorization header present")
+        raise HTTPException(status_code=401,
+                            detail="No Authorization header present")
 
-    log.warning(settings.DEBUG)
-    log.warning(type(settings.DEBUG))
+    try:
+        ckan = RemoteCKAN(settings.API_URL, apikey=authorization)
+        user = ckan.call_action("user_show", {'id': user_id})
+    except NotFound as e:
+        log.exception(e)
+        raise HTTPException(status_code=404,
+                            detail="User not found")
 
-    log.debug("Authorization header extracted from request headers")
-
-    if settings.DEBUG:
-        user_info = {
-            "id": "334cee1e-6afa-4639-88a2-f980e6ff42c3",
-            "name": "admin",
-            "display_name": "EnviDat Admin",
-            "email": "envidat@wsl.ch",
-            "sysadmin": True,
-        }
-    else:
-        ckan = ckanapi.RemoteCKAN(settings.API_URL, apikey=authorization)
-        user_info = ckan.call_action("user_show")
-
-    return user_info
+    return user
 
 
-def get_admin(user_info=Depends(get_user)):
+# TODO revew
+# def get_user(authorization: Annotated[str | None, Header()] = None):
+#     """Standard CKAN user."""
+#     if not settings.DEBUG and not authorization:
+#         log.error("No Authorization header present")
+#         raise HTTPException(status_code=401,
+#         detail="No Authorization header present")
+#
+#     log.warning(settings.DEBUG)
+#     log.warning(type(settings.DEBUG))
+#
+#     log.debug("Authorization header extracted from request headers")
+#
+#     if settings.DEBUG:
+#         user_info = {
+#             "name": "admin",
+#             "display_name": "EnviDat Admin",
+#             "email": "envidat@wsl.ch",
+#             "sysadmin": True,
+#         }
+#     else:
+#         ckan = ckanapi.RemoteCKAN(settings.API_URL, apikey=authorization)
+#         user_info = ckan.call_action("user_show")
+#
+#     return user_info
+
+
+def get_admin(user=Depends(get_user)):
     """Admin CKAN user."""
     # Determine if is an admin
-    admin = user_info.get("sysadmin", False)
+    admin = user.get("sysadmin", False)
 
     if not admin:
-        log.error(f"User {user_info} is not an admin")
-        raise HTTPException(status_code=401, detail=f"User {user_info} is not an admin")
+        log.error(f"User {user} is not an admin")
+        raise HTTPException(status_code=401,
+                            detail=f"User {user} is not an admin")
 
-    return user_info
+    return user
+
+
+# TODO review
+# def get_admin(user_info=Depends(get_user)):
+#     """Admin CKAN user."""
+#     # Determine if is an admin
+#     admin = user_info.get("sysadmin", False)
+#
+#     if not admin:
+#         log.error(f"User {user_info} is not an admin")
+#         raise HTTPException(status_code=401,
+#                             detail=f"User {user_info} is not an admin")
+#
+#     return user_info
 
 
 def get_token(authorization: Annotated[str | None, Header()] = None):
