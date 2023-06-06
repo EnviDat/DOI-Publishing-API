5449379c3bf224c5a9195f2f22e7ef43b726a26f
RebeccaKurupBuchholz <rebecca.kurup@wsl.ch>
2023-06-06 14:52:06 +0200

    feat: implement ckan cookie test comments

--- app/api/datacite.py
+++ app/api/datacite.py
@@ -1,24 +1,22 @@
 """DataCite API Router"""
 
-
 from typing import Annotated
 # from fastapi import Depends, Header
-from fastapi import APIRouter, HTTPException, Security, Response, Query
-from fastapi.security.api_key import APIKeyHeader
+from fastapi import APIRouter, HTTPException, Security, Response, Query, Cookie
+from fastapi.security.api_key import APIKeyHeader, APIKeyCookie
 
 from app.auth import get_user
 from app.logic.datacite import reserve_draft_doi_datacite, DoiSuccess, \
     DoiErrors
 from app.logic.remote_ckan import get_ckan_package_show
 
-
 # Setup logging
 import logging
+
 log = logging.getLogger(__name__)
 
-
 # TODO verify signout
-# TODO review exception formatting
+# TODO review exception formatting, consider returning generic response
 # TODO add Swagger documentation of endpoints (title, description,
 #  query parameters etc.)
 # TODO implement authorization
@@ -30,10 +28,19 @@ router = APIRouter(prefix="/datacite", tags=["datacite"]
                    # dependencies=[Depends(get_user)]
                    )
 
-# Setup authorization
-authorization_header = APIKeyHeader(name='Authorization')
+# Setup authorization header
+authorization_header = APIKeyHeader(name='Authorization',
+                                    description='ckan cookie for logged in '
+                                                'user passed in authorization '
+                                                'header')
+
+# Setup ckan cookie used for authorization
+ckan_cookie = APIKeyCookie(name="ckan",
+                           description="ckan cookie for logged in user")
 
 
+# TODO review if authorizations should use cookie or header,
+#  NOTE: cookie may not work when sending to an external site
 # TODO remove endpoint after testing and move to logic/datacite.py,
 #  call from doi/create_doi_draft
 # TODO potentially remove responses, response arg
@@ -57,27 +64,32 @@ def reserve_draft_doi(
         package: Annotated[str, Query(alias="package-id",
                                       description="CKAN package id or name")],
         response: Response,
-        authorization: str = Security(authorization_header)
+        # ckan: str = Security(ckan_cookie),
+        authorization: str = Security(authorization_header),
 ):
     """
     Authenticate user, extract DOI from package, and reserve draft DOI in
     DataCite.
     """
 
-    # Authorize user, if user invalid then HTTPException raised
+    # Authorize user, if user invalid then raises HTTPException
     get_user(user, authorization)
+    # user_info = get_user(user, ckan)
 
     # TODO clarify if doi will already be assigned to package
     #  or should be passed as arg
-    # Get package and extract doi
+    # Get package
+    # If package id invalid or user not authorized then raises HTTPException
     package = get_ckan_package_show(package, authorization)
+
+    # Extract doi
     doi = package.get('doi')
     if not doi:
         raise HTTPException(status_code=500,
                             detail="Package does not have a doi")
 
     # TODO remove test_doi
-    test_doi = "10.16904/envidat.test19"
+    test_doi = "10.16904/envidat.test20"
 
     # TODO revert to calling DataCite API with doi, test
     # Reserve DOI in "Draft" state with DataCite,
