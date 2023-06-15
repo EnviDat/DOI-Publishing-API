f7057fbd19174f4bd129b15cbb00eebe392e3569
RebeccaKurupBuchholz <rebecca.kurup@wsl.ch>
2023-06-15 11:24:29 +0200

    feat: add external_doi util for getting external platform

---
+++ app/logic/external_doi/constants.py
@@ -0,0 +1,17 @@
+"""Constants used by envidat_doi module"""
+
+from enum import Enum
+
+
+class ExternalPlatform(str, Enum):
+    ZENODO = "zenodo"
+
+
+EXTERNAL_PLATFORM_NAMES = {
+    "zenodo": ExternalPlatform.ZENODO
+}
+
+EXTERNAL_PLATFORM_PREFIXES = {
+    "10.5281": ExternalPlatform.ZENODO
+}
+
---
+++ app/logic/external_doi/utils.py
@@ -0,0 +1,32 @@
+"""Utils for envidat_doi module"""
+
+from app.logic.external_doi.constants import ExternalPlatform, \
+    EXTERNAL_PLATFORM_NAMES, EXTERNAL_PLATFORM_PREFIXES
+
+
+def get_doi_external_platform(doi: str) -> ExternalPlatform | None:
+    """
+    Return ExternalPlatform that most likely corresponds to input doi string.
+
+    If external platform not found then returns None.
+
+    Args:
+        doi (str): Input doi string
+
+    Returns:
+        ExternalPlatform | None
+    """
+
+    # Search by names for doi that corresponds to supported external platform
+    for key, value in EXTERNAL_PLATFORM_NAMES.items():
+        if key in doi:
+            return value
+
+    # Search by DOI prefixes for doi that corresponds to
+    # supported external platforms
+    for key, value in EXTERNAL_PLATFORM_PREFIXES.items():
+        if key in doi:
+            return value
+
+    return None
+
