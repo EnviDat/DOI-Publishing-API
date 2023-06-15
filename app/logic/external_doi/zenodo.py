efd2aa84f2069c72bb302ffc8425fa4c4f121d2d
RebeccaKurupBuchholz <rebecca.kurup@wsl.ch>
2023-06-15 16:29:02 +0200

    feat: add zenodo converter handler

--- app/logic/external_doi/zenodo.py
+++ app/logic/external_doi/zenodo.py
@@ -5,7 +5,9 @@ import requests
 
 
 # TODO review error messages
-def convert_zenodo_doi(doi: str) -> dict:
+def convert_zenodo_doi(
+        doi: str, add_placeholders: bool = False
+) -> dict:
     """
     Return metadata for input doi and convert metadata to EnviDat
     CKAN package format.
@@ -19,6 +21,9 @@ def convert_zenodo_doi(doi: str) -> dict:
 
     Args:
         doi (str): Input doi string
+        add_placeholders (bool): If true placeholder values are added for
+                         required EnviDat package fields.
+                         Default value is False.
 
     Returns:
         dict: Dictionary with metadata in EnviDat CKAN package
@@ -29,8 +34,8 @@ def convert_zenodo_doi(doi: str) -> dict:
     if not record_id:
         return {
             "status_code": 400,
-            "error": "Cannot extract record ID from input Zenodo DOI",
-            "message": f"The following DOI was not found: {doi}"
+            "message": f"The following DOI was not found: {doi}",
+            "error": "Cannot extract record ID from input Zenodo DOI"
         }
 
     with open("app/config/zenodo.json", "r") as zenodo_config:
@@ -41,8 +46,8 @@ def convert_zenodo_doi(doi: str) -> dict:
         # TODO email admin config error
         return {
             "status_code": 500,
-            "error": "Cannot not get externalApi.zenodoRecords from config",
-            "message": "Cannot process DOI. Please contact EnviDat team."
+            "message": "Cannot process DOI. Please contact EnviDat team.",
+            "error": "Cannot not get externalApi.zenodoRecords from config"
         }
 
     api_url = f"{records_url}/{record_id}"
@@ -54,7 +59,16 @@ def convert_zenodo_doi(doi: str) -> dict:
     # TODO handle non 200 status code on response from zenodo
     # TODO convert Zenodo response to EnviDat format
 
-    return response.json()
+    if response.status_code != 200:
+        return {
+            "status_code": response.status_code,
+            "message": f"The following DOI was not found: {doi}",
+            "error": response.json()
+        }
+
+    envidat_record = convert_zenodo_to_envidat(response.json())
+
+    return envidat_record
 
 
 def get_zenodo_record_id(doi: str) -> str | None:
@@ -83,4 +97,36 @@ def get_zenodo_record_id(doi: str) -> str | None:
         return None
 
     return record_id
+
+
+def convert_zenodo_to_envidat(
+        metadata: dict, add_placeholders: bool = False
+) -> dict:
+    """
+    Convert Zenodo record dictionary to EnviDat CKAN package format.
+
+    If add_placeholders true then add default values from config.
+    Values added are required by EnviDat CKAN to create a new package.
+
+     Args:
+        metadata (dict): Response data object from Zenodo API call
+        add_placeholders (bool): If true placeholder values are added for
+                         required EnviDat package fields.
+                         Default value is False.
+    """
+
+    # Assign dictionary to contain values converted from Zenodo
+    # to EnviDat CKAN package format
+    pkg = {}
+
+    data = metadata.get("metadata")
+
+    # TODO determine if DOI should be validated
+
+    # TODO start dev here
+    # TODO convert creators to EnviDat authors format
+    creators = data.get("creators")
+
+    return creators
+
 
