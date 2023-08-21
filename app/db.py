bcd67131b3d8de3a838227e813d507e2d0b2a8a3
sam <sam.woodcock@protonmail.com>
2023-08-21 13:08:05 +0100

    refactor: add logs to db init

--- app/db.py
+++ app/db.py
@@ -1,9 +1,13 @@
 """Config file for TortoiseORM and database init."""
 
+import logging
+
 from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import settings
+
+log = logging.getLogger(__name__)
 
 TORTOISE_ORM = {
     "connections": {"default": settings.DB_URI},
@@ -23,6 +27,7 @@ TORTOISE_ORM = {
 
 def init_db(app: FastAPI) -> None:
     """Register and create database connection."""
+    log.debug(f"Connecting to DB: {settings.DB_URI}")
     register_tortoise(
         app,
         config=TORTOISE_ORM,
